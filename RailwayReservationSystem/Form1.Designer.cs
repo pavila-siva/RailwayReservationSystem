e7ef6ec1bf0ca77a47aaec35d943b8361da53d23
pavila <pavilasivar@gmail.com>
2025-01-18 19:43:05 +0530

    Add project files.

---
+++ RailwayReservationSystem/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace RailwayReservationSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            Welcome = new Label();
+            SuspendLayout();
+            // 
+            // guna2CircleButton1
+            // 
+            guna2CircleButton1.DisabledState.BorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2CircleButton1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2CircleButton1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2CircleButton1.FillColor = Color.ForestGreen;
+            guna2CircleButton1.Font = new Font("Segoe UI", 9F);
+            guna2CircleButton1.ForeColor = Color.White;
+            guna2CircleButton1.Location = new Point(-123, -3);
+            guna2CircleButton1.Name = "guna2CircleButton1";
+            guna2CircleButton1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            guna2CircleButton1.Size = new Size(251, 506);
+            guna2CircleButton1.TabIndex = 0;
+            // 
+            // Welcome
+            // 
+            Welcome.AutoSize = true;
+            Welcome.Font = new Font("Century Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Welcome.ForeColor = Color.DimGray;
+            Welcome.Location = new Point(195, 25);
+            Welcome.Name = "Welcome";
+            Welcome.Size = new Size(156, 37);
+            Welcome.TabIndex = 1;
+            Welcome.Text = "Welcome";
+            Welcome.Click += Welcome_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(470, 254);
+            Controls.Add(Welcome);
+            Controls.Add(guna2CircleButton1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Label Welcome;
+    }
+}
---
+++ RailwayReservationSystem/Form1.cs
@@ -0,0 +1,15 @@
+namespace RailwayReservationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Welcome_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
